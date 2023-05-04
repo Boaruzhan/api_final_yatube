eb00398c0ff1dbc19e3132f55555a4a23c13e83c
Iosif Ulinetz <ulinetz.iosif@yandex.ru>
2023-05-05 02:45:54 +0300

    third try.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
 
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
-
 router.register(r'posts', PostViewSet, basename='posts')
 router.register(r'groups', GroupViewSet, basename='groups')
 router.register(
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404
+
 from posts.models import Group, Post, User
+
 from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 
 from .permissions import IsPostAuthor
-from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+from .serializers import (CommentSerializer,
+                          FollowSerializer,
+                          GroupSerializer,
                           PostSerializer)
 
 
